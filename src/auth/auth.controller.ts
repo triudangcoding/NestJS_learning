e7b805b70558d965a8fa8af079bbfbca9b95247d
trieudangcoding <trieudangcoding@gmail.com>
2025-10-17 00:59:46 +0700

    Enhance AuthController by adding HTTP status codes for register, login, and refresh-token endpoints, ensuring consistent response handling across authentication processes.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginBodyDTO, RegisterBodyDTO, LoginResponseDto, RefreshTokenBodyDTO } from "./dto/login-body.dto";
 @Controller("auth")
@@ -6,16 +6,19 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("register")
+  @HttpCode(HttpStatus.OK)
   async register(@Body() body: RegisterBodyDTO) {
     return this.authService.register(body);
   }
 
   @Post("login")
+  @HttpCode(HttpStatus.OK)
   async login(@Body() body: LoginBodyDTO) {
     return new LoginResponseDto( await this.authService.login(body));
   }
 
   @Post("refresh-token")
+  @HttpCode(HttpStatus.OK)
   async refreshToken(@Body() body: RefreshTokenBodyDTO) {
     return this.authService.refreshToken(body);
   }
